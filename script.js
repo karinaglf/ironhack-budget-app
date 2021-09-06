236c36736cc5e5dffb52da7f1ca4fc5d9eed1afe
Karina Freitas <karinaglf@gmail.com>
2021-09-07 00:07:43 +0100

    add first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+//document.getElementById('')
+//document.querySelector('');
+
+// Total Balance
+const balanceElement = document.getElementById('balance');
+// Value Input
+const valueInputElement = document.querySelector('input');
+// Income or Expense
+const selectElement = document.querySelector('select');
+// Add Button
+const addButtonElement = document.querySelector('footer button');
+// Entry list
+const entryListElement = document.querySelector('section ul');
+
+function addEntry (amount, operation) {
+    const listEntry = document.createElement('li');
+    listEntry.classList.add(operation);
+    
+    const listEntryValue = document.createElement('strong');
+    listEntryValue.innerText = amount + "$";
+
+    const listEntryDescription = document.createElement('em');
+    listEntryDescription.innerText = "Description";
+
+    const listEntryOperator = document.createElement('span');
+   
+    if (operation === 'income') {
+        listEntryOperator.innerText = "+";
+    } else if (operation === 'expense') {
+        listEntryOperator.innerText = "-";
+    };
+
+    listEntry.appendChild(listEntryDescription);
+    listEntry.appendChild(listEntryOperator);
+    listEntry.appendChild(listEntryValue);
+
+    entryListElement.appendChild(listEntry);
+};
+
+function updateBalance () {    
+    let total = 0;
+
+    for (let item of entryListElement.children) {
+        const valueElement = item.querySelector('strong');
+        const operationElement = item.querySelector('span');
+
+        const value = parseInt(valueElement.innerText);
+        const operation = operationElement.innerText;
+
+        if (operation === "+") {
+            total = total + value;
+        } else if (operation === "-") {
+            total = total - value;
+        };
+    };
+
+    balanceElement.innerText = total + "$";
+
+};
+
+addButtonElement.onclick = function () {
+    const amount = valueInputElement.value;
+    const operation = selectElement.value;
+    
+    addEntry(amount, operation);
+
+    valueInputElement.value = "";
+
+    updateBalance();
+};
+
